d9ce20335350a9098b42a2861b02dd2b3b73a7f3
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-03-09 20:42:26 -0300

    feat: implement Firebase Firestore connection check and update UI status

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
+import {db} from './services/Firebase.ts'
+import {getDocs, collection} from 'firebase/firestore'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firestoreStatus, setFirestoreStatus] = useState('Firebase Firestore is not connected!')
+  const firestore = db;
+
+  useEffect(() => {
+    async function checkFirestoreConn() {
+      const querySnapshot = await getDocs(collection(firestore, 'status'));
+      querySnapshot.forEach(doc => {
+        setFirestoreStatus(doc.data().result);
+      })
+    }
+
+    checkFirestoreConn();
+  })
 
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo"/>
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo"/>
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>GameBlame frontend is running!</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+          Firebase Firestore status: </p>
+        <p className="read-the-docs">
+          {firestoreStatus}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
